1df932ccb2c748077328ce7c86ea1faf75aa4251
Yangyang Cui <cuiyang21@gmail.com>
2019-04-19 19:59:29 -0500

    works well,v2,10:48

--- public/js/main.js
+++ public/js/main.js
@@ -24,8 +24,8 @@ function loadBackgroundSprites() {
     return loadImage('/img/tiles.png')
         .then(image => {
             const sprites = new SpriteSheet(image, 16, 16);
-            sprites.define('ground', 0, 0);
-            sprites.define('sky', 3, 23);
+            sprites.defineTile('ground', 0, 0);
+            sprites.defineTile('sky', 3, 23);
             return sprites;
         });
 }
