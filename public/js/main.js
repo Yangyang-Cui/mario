44b281bae008e031f0bfe9832753ac494c135b8c
Yangyang Cui <cuiyang21@gmail.com>
2019-04-19 19:13:10 -0500

    add first time

---
+++ public/js/main.js
@@ -0,0 +1,46 @@
+import SpriteSheet from './SpriteSheet.js';
+import { loadImage, loadLevel } from './loader.js';
+
+function drawBackground(background, context, sprites) {
+    background.ranges.forEach(([x1, x2, y1, y2]) => {
+        for (let x = x1; x < x2; ++x) {
+            for (let y = y1; y < y2; ++y) {
+                sprites.drawTile(background.tile, context, x, y);
+            }
+        };
+    });
+}
+
+function loadMarioSprites() {
+    return loadImage('/img/characters.gif')
+        .then(image => {
+            const sprites = new SpriteSheet(image, 16, 16);
+            sprites.define('idle', 16, 3);
+            return sprites;
+        });
+}
+
+function loadBackgroundSprites() {
+    return loadImage('/img/tiles.png')
+        .then(image => {
+            const sprites = new SpriteSheet(image, 16, 16);
+            sprites.define('ground', 0, 0);
+            sprites.define('sky', 3, 23);
+            return sprites;
+        });
+}
+
+const canvas = document.getElementById('screen');
+const context = canvas.getContext('2d');
+
+Promise.all([
+        loadMarioSprites(),
+        loadBackgroundSprites(),
+        loadLevel('1-1'),
+    ])
+    .then(([marioSprite, sprites, level]) => {
+        level.backgrounds.forEach(background => {
+            drawBackground(background, context, sprites);
+        });
+        marioSprite.draw('idle', context, 64, 64);
+    });
