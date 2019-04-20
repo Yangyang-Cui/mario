8f7fe3c043910986cd8424bd2538800914f18f48
Yangyang Cui <cuiyang21@gmail.com>
2019-04-20 11:40:27 -0500

    ww,v3,16:35

--- public/js/main.js
+++ public/js/main.js
@@ -15,18 +15,26 @@ Promise.all([
     .then(([mario, backgroundSprites, level]) => {
         const comp = new Compositor();
         const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
-        comp.layers.push(backgroundLayer);
+        // comp.layers.push(backgroundLayer);
 
         const gravity = 0.5;
 
         const spriteLayer = createSpriteLayer(mario);
         comp.layers.push(spriteLayer);
 
-        function update() {
+        let deltaTime = 0;
+        let lastTime = 0;
+
+        function update(time) {
+            deltaTime = time - lastTime;
+            console.log(deltaTime, time);
+
             comp.draw(context);
-            mario.update();
+            mario.update(deltaTime);
             mario.vel.y += gravity;
             requestAnimationFrame(update);
+            //setTimeout(update, 1000 / 144);
+            lastTime = time;
         }
         update();
     });
