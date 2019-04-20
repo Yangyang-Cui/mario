a0af97e52cdac41c79c55b72a889e41c02fff235
Yangyang Cui <cuiyang21@gmail.com>
2019-04-20 11:27:28 -0500

    ww,v3,12:33

--- public/js/main.js
+++ public/js/main.js
@@ -1,56 +1,23 @@
 import Compositor from './Compositor.js';
 import { loadLevel } from './loader.js';
-import { loadMarioSprites, loadBackgroundSprites } from './sprites.js';
-import { createBackgroundLayer } from './layers.js';
+import { createMario } from './entities.js';
+import { loadBackgroundSprites } from './sprites.js';
+import { createBackgroundLayer, createSpriteLayer } from './layers.js';
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
 
-function createSpriteLayer(entity) {
-    return function drawSpriteLayer(context) {
-        entity.draw(context);
-    };
-}
-
-class Vec2 {
-    constructor(x, y) {
-        this.set(x, y);
-    }
-    set(x, y) {
-        this.x = x;
-        this.y = y;
-    }
-}
-
-class Entity {
-    constructor() {
-        this.pos = new Vec2(0, 0);
-        this.vel = new Vec2(0, 0);
-    }
-}
-
 Promise.all([
-        loadMarioSprites(),
+        createMario(),
         loadBackgroundSprites(),
         loadLevel('1-1'),
     ])
-    .then(([marioSprite, backgroundSprites, level]) => {
+    .then(([mario, backgroundSprites, level]) => {
         const comp = new Compositor();
         const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
         comp.layers.push(backgroundLayer);
 
         const gravity = 0.5;
-
-        const mario = new Entity();
-        mario.pos.set(64, 180);
-        mario.vel.set(2, -10);
-        mario.draw = function drawMario(context) {
-            marioSprite.draw('idle', context, this.pos.x, this.pos.y);
-        }
-        mario.update = function updateMario() {
-            this.pos.x += this.vel.x;
-            this.pos.y += this.vel.y;
-        }
 
         const spriteLayer = createSpriteLayer(mario);
         comp.layers.push(spriteLayer);
