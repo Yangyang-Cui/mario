0ff2ff429e9db1cb1202ee31dc11cf997d2991b9
Yangyang Cui <cuiyang21@gmail.com>
2019-04-21 09:44:58 -0500

    difference, but code still works; v5; 50:11

--- public/js/layers.js
+++ public/js/layers.js
@@ -29,14 +29,18 @@ export function createCollisionLayer(level) {
     const tileSize = tileResolver.tileSize;
 
     const getByIndexOriginal = tileResolver.getByIndex;
+
     tileResolver.getByIndex = function getByIndexFake(x, y) {
         resolvedTiles.push({ x, y });
         return getByIndexOriginal.call(tileResolver, x, y);
     }
 
     return function drawCollision(context) {
+        context.strokeStyle = 'blue';
         resolvedTiles.forEach(({ x, y }) => {
-            console.log('would draw', x, y);
+            context.beginPath();
+            context.rect(x * tileSize, y * tileSize, tileSize, tileSize);
+            context.stroke();
         });
 
         resolvedTiles.length = 0;
