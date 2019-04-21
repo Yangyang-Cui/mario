91bb55f3c60d4461219359dbd0cadfde5c3cfc5e
Yangyang Cui <cuiyang21@gmail.com>
2019-04-21 10:41:12 -0500

    d, no bug; v5; 01:03:02;

--- public/js/entities.js
+++ public/js/entities.js
@@ -7,7 +7,7 @@ export function createMario() {
     return loadMarioSprites()
         .then(sprite => {
             const mario = new Entity();
-            mario.size.set(14, 16);
+            mario.size.set(14, 48);
 
             mario.addTrait(new Jump());
             mario.addTrait(new Velocity());
